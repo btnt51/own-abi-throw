8d8678aeec04ae747a3236b7b10129e24e233ef3
btnt51 <51btnt51@gmail.com>
2021-08-08 14:46:50 +0300

    Updating myabicpp.cpp, updated structers, in personal function added new loop 'for' for printing information about LSDA call site

--- src/myabicpp.cpp
+++ src/myabicpp.cpp
@@ -71,26 +71,35 @@ void __cxa_end_catch()
 
 
 struct LSDA_Header{
-    uint8_t start_encoding;
-    uint8_t type_encoding;
-    uint8_t ttype;
+    uint8_t lsda_start_encoding;
+    uint8_t lsda_type_encoding;
+    uint8_t lsda_call_site_table_length;
 
 };
 
 
-struct LSDA_CS_Header
+struct LSDA_Call_Site_Header
 {
     uint8_t encoding;
     uint8_t length;
 };
 
 
-struct LSDA_CS
+struct LSDA_Call_Site
 {
-    uint8_t start;
-    uint8_t len;
-    uint8_t lp;
-    uint8_t action;
+    LSDA_Call_Site(const uint8_t *ptr)
+    {
+        cs_start = ptr[0];
+        cs_len = ptr[1];
+        cs_lp = ptr[2];
+        cs_action = ptr[3];
+    }
+
+
+    uint8_t cs_start;
+    uint8_t cs_len;
+    uint8_t cs_lp;
+    uint8_t cs_action;
 };
 
 
@@ -106,6 +115,24 @@ _Unwind_Reason_Code __gxx_personality_v0 (
     } else if (actions & _UA_CLEANUP_PHASE) {
         printf("Personality function, cleanup\n");
         const uint8_t* lsda = (const uint8_t*)_Unwind_GetLanguageSpecificData(context);
+        LSDA_Header *header = (LSDA_Header *)(lsda);
+        LSDA_Call_Site_Header *cs_header = (LSDA_Call_Site_Header*)(lsda + sizeof(LSDA_Header));
+
+        size_t cs_in_table = cs_header->length / sizeof(LSDA_Call_Site);
+
+        const uint8_t *cs_table_base = lsda + sizeof(LSDA_Header) + sizeof(LSDA_Call_Site_Header);
+
+        for(size_t i = 0; i < cs_in_table; i++)
+        {
+            const uint8_t *offset = &cs_table_base[i * sizeof(LSDA_Call_Site)];
+            LSDA_Call_Site cs(offset);
+            printf("Found a CS:\n");
+            printf("\tcs_start: %i\n", cs.cs_start);
+            printf("\tcs_len: %i\n", cs.cs_len);
+            printf("\tcs_lp: %i\n", cs.cs_lp);
+            printf("\tcs_action: %i\n", cs.cs_action);
+        }
+
         uintptr_t ip = _Unwind_GetIP(context) -1;
         uintptr_t funcStart = _Unwind_GetRegionStart(context);
         uintptr_t offset = ip - funcStart;
